234eaf2e32645710da72f8ed7c957a59fc9765fe
Jeremiah <jeremiah@ideonic.com>
2016-09-13 16:25:01 +0800

    add delete button

--- client.js
+++ client.js
@@ -10,6 +10,12 @@ $(function () {
     $('#add-peanut').trigger('reset') // clear the form
     addPeanut(data) // send ajax request
   })
+
+  // Add a click listener so we can delete peanuts
+  $('main').on('click', '.delete-peanut', function () {
+    var id = $(this).parent().attr('id')
+    deletePeanut(id)
+  })
 })
 
 // GET /peanuts
@@ -21,10 +27,15 @@ function listPeanuts () {
     console.log('success listing peanuts')
     $('main').empty()
     data.forEach(function (elem, index) {
-      $('main').append('<div class="card"  id="' + index + '">' +
+      $('main').append('<div class="card peanut">' +
         '<div class="card-header">' + elem.name + '</div>' +
-        '<div class="card-block">' + elem.cost + '</div>' +
-        '</div>')
+        '<div class="card-block" >' +
+          '<p class="card-text">' + elem.cost + '</p>' +
+            '<div class="btn-group" id="' + index + '">' +
+              '<button class="btn btn-danger delete-peanut">Delete</button>' +
+            '</div>' +
+          '</div>' +
+      '</div>')
     })
   }).fail(function () {
     console.log('error listing peanuts')
@@ -43,5 +54,19 @@ function addPeanut (peanutData) {
     listPeanuts()
   }).fail(function () {
     console.log('error adding peanut')
+  })
+}
+
+// DELETE /peanuts/:id
+function deletePeanut (peanutId) {
+  $.ajax({
+    url: 'http://localhost:3000/peanuts/' + peanutId,
+    type: 'DELETE'
+  }).done(function (data) {
+    console.log('success deleting peanut')
+    // if we wanted to be efficient we would just append the new peanut but for ease we will instead just redisplay the list
+    listPeanuts()
+  }).fail(function () {
+    console.log('error deleting peanut')
   })
 }
