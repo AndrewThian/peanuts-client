41c17afdf4d1a5b55985928e8e7e143d7bda4dc1
Jeremiah <jeremiah@ideonic.com>
2016-09-13 15:33:52 +0800

    add create form

--- client.js
+++ client.js
@@ -1,24 +1,47 @@
 /* globals $ */
 $(function () {
+  // when the page first loads we want to get all peanuts and display them
+  listPeanuts()
+
+  // we add form is clicked we post a request to create a new peanut
+  $('#add-peanut').submit(function (event) {
+    event.preventDefault() // stop the browser reloading
+    var data = $('#add-peanut').serialize() // extract the data from the form
+    $('#add-peanut').trigger('reset') // clear the form
+    addPeanut(data) // send ajax request
+  })
+})
+
+// GET /peanuts
+function listPeanuts () {
   $.ajax({
     url: 'http://localhost:3000/peanuts',
     type: 'GET'
+  }).done(function (data) {
+    console.log('success listing peanuts')
+    $('main').empty()
+    data.forEach(function (elem, index) {
+      $('main').append('<div class="card"  id="' + index + '">' +
+        '<div class="card-header">' + elem.name + '</div>' +
+        '<div class="card-block">' + elem.cost + '</div>' +
+        '</div>')
+    })
+  }).fail(function () {
+    console.log('error listing peanuts')
   })
-    .done(function (data) {
-      console.log('success')
+}
 
-      $('main').empty()
-      data.forEach(function (elem, index) {
-        $('main').append('<div class="card"  id="' + index + '">' +
-          '<div class="card-header">' + elem.name + '</div>' +
-          '<div class="card-block">' + elem.cost + '</div>' +
-          '</div>')
-      })
-    })
-    .fail(function () {
-      console.log('error')
-    })
-    .always(function () {
-      console.log('complete')
-    })
-})
+// POST /peanuts
+function addPeanut (peanutData) {
+  $.ajax({
+    url: 'http://localhost:3000/peanuts',
+    type: 'POST',
+    data: peanutData
+  }).done(function (data) {
+    console.log('success adding peanut')
+    // if we wanted to be efficient we would just append the new peanut but for ease we will instead just redisplay the list
+    listPeanuts()
+  }).fail(function () {
+    console.log('error adding peanut')
+  })
+}
